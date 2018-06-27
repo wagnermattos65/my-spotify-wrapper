e115089238d2e06d31b34c66eeb4c42a47ae8a89
Wagner Mattos <wagner_mattos@msn.com>
2018-06-27 17:04:22 -0300

    change card color on example file

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const API_URL = 'https://api.spotify.com/v1';
 
-const TOKEN_API = 'BQB0cMTRU4MZG5X7IksO7JwWcGiUhMISL1bg93CorCYQg1rVRwejaIt4Hn6-3NN6N58JQXbZOzuGkECw76XYRRc29uCA-yC0fF96aq1HCMXyQbg3y00hD2_huLVg2eFNLmiuJKkSHeezOP9Qdb9iLwbwGi9G1Qw';
+const TOKEN_API = 'BQBtaerEoofX-0I3MHXs07qMD0CjlAgmV8wGUuiQQqYrb1YBupdsayVWbBHMF8XxPhm7WopqVmZVt3RgGjdggun18gqsr8PLLjjba6F5AL23pezFOUgAZX8SVv7a5RgZsHc__eiUcCCBkKM8pAIznPTXBl-o47E';
 
 const HEADERS = {
   headers: {
